23307d2677d2cd127bb7b5bc2d3df8d39e082742
Clarice0997 <clarice09975@gmail.com>
2023-01-07 22:37:52 +0800

    feat: 用户接口返回用户数据总条数

--- src/model/userManagerModel.js
+++ src/model/userManagerModel.js
@@ -13,6 +13,32 @@ const { formatDate } = require('../utils/formatDate')
 const dotenv = require('dotenv')
 dotenv.config('./.env')
 
+// 查询数据库用户条数
+function getCountUser(callback) {
+  querySelect(`select count(uid) totalData from users where role in (1,2)`, (err, data) => {
+    // 判断查询是否出错
+    if (err != null) {
+      console.log('数据库查询出错:' + err)
+      // 返回错误信息
+      callback({
+        code: 412,
+        message: '查询数据库出错'
+      })
+    }
+    if (data.length != 0) {
+      callback({
+        code: 200,
+        data: data
+      })
+    } else {
+      callback({
+        code: 412,
+        message: '查询数据库出错'
+      })
+    }
+  })
+}
+
 /**
  * 获取用户信息函数
  * @param {*} pageNumber 页码
@@ -20,7 +46,7 @@ dotenv.config('./.env')
  * @param {*} callback 回调函数
  * @returns
  */
-function getUsersInfo(pageNumber, pageSize, callback) {
+async function getUsersInfo(pageNumber, pageSize, callback) {
   // 判断是否存在页码
   if (typeof pageNumber == 'undefined') {
     pageNumber = 1
@@ -29,7 +55,23 @@ function getUsersInfo(pageNumber, pageSize, callback) {
   if (typeof pageSize == 'undefined') {
     pageSize = 10
   }
-  querySelect(`select d.* from users u left join users_detail d on u.uid=d.uid where role = 1 limit ${pageNumber - 1},${pageSize}`, (err, data) => {
+  // 数据总条数
+  let count = 0
+
+  // 获取总用户条数
+  await getCountUser(result => {
+    if (result.code === 200) {
+      count = result.data[0].totalData
+    } else {
+      callback({
+        code: 412,
+        message: '查询数据库出错'
+      })
+    }
+  })
+
+  // 查询用户数据
+  querySelect(`select d.* from users u left join users_detail d on u.uid=d.uid where role = 1 limit ${(pageNumber - 1) * pageSize},${pageSize}`, (err, data) => {
     // 判断查询是否出错
     if (err != null) {
       console.log('数据库查询出错:' + err)
@@ -46,7 +88,8 @@ function getUsersInfo(pageNumber, pageSize, callback) {
       })
       callback({
         code: 200,
-        data
+        data,
+        count
       })
     } else {
       callback({
@@ -74,32 +117,42 @@ function searchUser(uid, nickname, pageNumber, pageSize, callback) {
   if (typeof pageSize == 'undefined') {
     pageSize = 10
   }
-  querySelect(`select d.* from users u left join users_detail d on u.uid = d.uid where role = 1 ${uid && nickname ? `and d.uid = ${uid} and d.nickname like '%${nickname}%'` : uid ? `and d.uid = ${uid}` : `and d.nickname like '%${nickname}%'`} limit ${pageNumber - 1},${pageSize}`, (err, data) => {
-    // 判断查询是否出错
-    if (err != null) {
-      console.log('数据库查询出错:' + err)
-      // 返回错误信息
-      callback({
-        code: 412,
-        message: '查询数据库出错'
-      })
-      return
+  // 数据总条数
+  let count = 0
+
+  querySelect(
+    `select d.* from users u left join users_detail d on u.uid = d.uid where role = 1 ${uid && nickname ? `and d.uid = ${uid} and d.nickname like '%${nickname}%'` : uid ? `and d.uid = ${uid}` : `and d.nickname like '%${nickname}%'`} limit ${(pageNumber - 1) * pageSize},${pageSize}`,
+    (err, data) => {
+      // 判断查询是否出错
+      if (err != null) {
+        console.log('数据库查询出错:' + err)
+        // 返回错误信息
+        callback({
+          code: 412,
+          message: '查询数据库出错'
+        })
+        return
+      }
+      if (data.length != 0) {
+        // 数据总条数
+        count = data.length
+        // 格式化时间
+        data.forEach(res => {
+          res.create_time = formatDate(new Date(res.create_time), 'yyyy年MM月dd日 hh时mm分')
+        })
+        callback({
+          code: 200,
+          data,
+          count
+        })
+      } else {
+        callback({
+          code: 412,
+          message: '不存在该用户'
+        })
+      }
     }
-    if (data.length != 0) {
-      data.forEach(res => {
-        res.create_time = formatDate(new Date(res.create_time), 'yyyy年MM月dd日 hh时mm分')
-      })
-      callback({
-        code: 200,
-        data
-      })
-    } else {
-      callback({
-        code: 412,
-        message: '不存在该用户'
-      })
-    }
-  })
+  )
 }
 
 // 导出模块
